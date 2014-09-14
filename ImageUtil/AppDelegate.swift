78a220bffcdb3eb6af39824ae838f4f66657e4e4
Jürgen <juergenbaumann@gmail.com>
2014-09-14 13:43:12 +0200

    - added TableView and helper functions

--- ImageUtil/AppDelegate.swift
+++ ImageUtil/AppDelegate.swift
@@ -8,12 +8,16 @@
 
 import Cocoa
 
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDataSource, NSTableViewDelegate {
 
+  var imageFolder : NSURL?
+  let fm = NSFileManager.defaultManager()
+  
+  
   @IBOutlet weak var window: NSWindow!
   @IBOutlet weak var debugLog: NSTextField!
-  var imageFolder : NSURL?
-
+  @IBOutlet weak var fileListTableView: NSTableView!
+ 
 
   func applicationDidFinishLaunching(aNotification: NSNotification?) {
     // Insert code here to initialize your application
@@ -33,9 +37,64 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     if openPanel.runModal() == NSOKButton
     {
       imageFolder = openPanel.URLs[0] as? NSURL
+      let x : NSURL = openPanel.directoryURL
       println("openPanel: \(openPanel.URLs)")
+      println("imageFolder: \(imageFolder)")
+      readMetaDataOfFilesInDirectory(imageFolder!)
+      
     }
     debugLog.stringValue = imageFolder?.description
+  }
+  
+  func numberOfRowsInTableView(aTableView: NSTableView!) -> Int
+  {
+    //let numberOfRows:Int = 20
+    let numberOfRows:Int = getDataArray().count
+    return numberOfRows
+  }
+  
+  func tableView(tableView: NSTableView!, objectValueForTableColumn tableColumn: NSTableColumn!, row: Int) -> AnyObject! {
+    //        var string:String = "row " + String(row) + ", Col" + String(tableColumn.identifier)
+    //        return string
+    var newString = getDataArray().objectAtIndex(row).objectForKey(tableColumn.identifier) as String
+    println("row:\(row), tableColumn.identifier:\(tableColumn.identifier), data:\(newString)")
+    return newString
+  }
+  
+  func getDataArray () -> NSArray {
+    var dataArray:[NSDictionary] = [["FileName": "Debasis", "Date": "Das"],
+      ["FileName": "Nishant", "Date": "Singh"],
+      ["FileName": "John", "Date": "Doe"],
+      ["FileName": "Jane", "Date": "Doe"],
+      ["FileName": "Mary", "Date": "Jane"]]
+    return dataArray
+  }
+  
+  func readMetaDataOfFilesInDirectory(dir:NSURL) {
+    let contents : NSArray = fm.contentsOfDirectoryAtURL(imageFolder!, includingPropertiesForKeys: nil, options: nil, error: nil)!
+    for content in contents {
+      println("\(content)")
+    }
+
+  }
+  
+  func parse(dateStr:String, format:String="yyyy-MM-dd") -> NSDate {
+    var dateFmt = NSDateFormatter()
+    dateFmt.timeZone = NSTimeZone.defaultTimeZone()
+    dateFmt.dateFormat = format
+    return dateFmt.dateFromString(dateStr)!
+  }
+  
+  func getDateTime(imageSource:CGImageSource) -> NSDate {
+    let uint:UInt = UInt.min
+    let metadataAtIndex = CGImageSourceCopyMetadataAtIndex(imageSource, uint, nil)
+    var imageDict : Dictionary  = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil)
+    let tiff : AnyObject = imageDict["{TIFF}"]!
+    let dateTime : String = tiff["DateTime"] as String
+    println("TypeName of dateTime = \(_stdlib_getTypeName(dateTime))")
+    println("dateTime:\(dateTime)")
+    return parse(dateTime, format:"yyyy:MM:dd HH:mm:ss")
+    
   }
 
 }
