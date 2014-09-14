099aef6e11b0733ce5a60b418649bbd5e4f8c509
Jürgen <juergenbaumann@gmail.com>
2014-09-14 10:10:07 +0200

    Initial Commit

---
+++ ImageUtil/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ImageUtil
+//
+//  Created by Juergen Baumann on 14.09.14.
+//  Copyright (c) 2014 Juergen Baumann. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification?) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification?) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ ImageUtilTests/ImageUtilTests.swift
@@ -0,0 +1,36 @@
+//
+//  ImageUtilTests.swift
+//  ImageUtilTests
+//
+//  Created by Juergen Baumann on 14.09.14.
+//  Copyright (c) 2014 Juergen Baumann. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class ImageUtilTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
