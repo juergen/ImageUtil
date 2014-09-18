81393d27da2de93a617e87b23e5ece14e9047231
Juergen Baumann <juba@symentis.com>
2014-09-18 10:23:29 +0200

    - fixed empty DateTime and image counter

--- ImageUtil/AppDelegate.swift
+++ ImageUtil/AppDelegate.swift
@@ -62,10 +62,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDataSource, NSTab
     let identifier = viewForTableColumn.identifier
     var cell = fileListTableView.makeViewWithIdentifier(identifier, owner: self) as NSTableCellView
     var value: AnyObject? = imageFileData[row].objectForKey(identifier)
-//    if (value == nil) {
-//      value = "./."
-//    }
-    if (value is NSDate) {
+    if (value! is NSDate) {
       let x = value as NSDate
       cell.textField.stringValue = x.description
     } else if (value is String) {
@@ -76,60 +73,65 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDataSource, NSTab
     return cell;
   }
   
-  @IBAction func rename(sender: NSButton) {
-    println("dateFromSelector: \(dateFromSelector.selectedRow)")
-    println("useNewStartDate: \(useNewStartDate.state)")
-    println("newStartDate: \(newStartDate.dateValue)")
-    println("hours: \(hours.stringValue)")
-    println("postfix: \(postfix.stringValue)")
-    println("appendOriginalName: \(appendOriginalName.state)")
-    
-    let useImageDate : Bool = (dateFromSelector.selectedRow < 1)
-    var dateStrings : Array = []
-    for image in imageFileData {
-      var counter : Int = 1
-      let date : NSDate = image["ImageDate"] as NSDate
-      var dateString : String = stringFromDate(date) + "\(stringFromDate(date))_\(counter++)"
-      dateStrings.append(dateString)
-      println("\(dateString)")
-    }
-
-  }
-  
-  
-  func readMetaDataOfFilesInDirectory(dir:NSURL) {
-    //
-    progressIndicator.doubleValue = 1
-    progressIndicator.startAnimation(self)
-    //
-    self.progressIndicator.startAnimation(self)
-    let contents : [NSURL] = fm.contentsOfDirectoryAtURL(imageFolder!,
-      includingPropertiesForKeys: [NSURLCreationDateKey],
-      options: .SkipsHiddenFiles,
-      error: nil) as [NSURL]
-    //
-    let count = contents.count
-    imageFileData = []
-    for (index, content : NSURL) in enumerate(contents) {
-      let progress : Double = 100 * (Double(index + 1) / Double(count))
-      println("progress: \(progress)")
-      progressIndicator.doubleValue = progress
-      println("nsurl: \(content.path)")
-      let cachedValues : Dictionary = content.resourceValuesForKeys([NSURLCreationDateKey], error: nil)!
-      let fileCreateDate : NSDate = cachedValues[NSURLCreationDateKey] as NSDate
-      let source = CGImageSourceCreateWithURL(content, nil)
-      if (nil == source) { continue }
-      imageFileData.append([
-        "FileName": content.lastPathComponent,
-        "ImageDate": getDateTime(source),
-        "FileDate": fileCreateDate
-        ]
-      )
-    }
-    progressIndicator.stopAnimation(self)
-
-  }
-  
+	@IBAction func rename(sender: NSButton) {
+		println("dateFromSelector: \(dateFromSelector.selectedRow)")
+		println("useNewStartDate: \(useNewStartDate.state)")
+		println("newStartDate: \(newStartDate.dateValue)")
+		println("hours: \(hours.stringValue)")
+		println("postfix: \(postfix.stringValue)")
+		println("appendOriginalName: \(appendOriginalName.state)")
+		
+		let useImageDate : Bool = (dateFromSelector.selectedRow < 1)
+		var dateStrings : Array<String> = []
+		for image in imageFileData {
+			var counter : Int = 1
+			let date : NSDate = image["ImageDate"] as NSDate
+			var dateString : String = "\(stringFromDate(date))_\(counter++)"
+			while (contains(dateStrings, dateString)) {
+				dateString = "\(stringFromDate(date))_\(counter++)"
+			}
+			dateStrings.append(dateString)
+			println("\(dateString)")
+		}
+	}
+	
+	
+	func readMetaDataOfFilesInDirectory(dir:NSURL) {
+		//
+		progressIndicator.doubleValue = 1
+		progressIndicator.startAnimation(self)
+		//
+		self.progressIndicator.startAnimation(self)
+		let contents : [NSURL] = fm.contentsOfDirectoryAtURL(imageFolder!,
+			includingPropertiesForKeys: [NSURLCreationDateKey],
+			options: .SkipsHiddenFiles,
+			error: nil) as [NSURL]
+		//
+		let count = contents.count
+		imageFileData = []
+		for (index, content : NSURL) in enumerate(contents) {
+			let progress : Double = 100 * (Double(index + 1) / Double(count))
+			//println("progress: \(progress)")
+			progressIndicator.doubleValue = progress
+			//println("nsurl: \(content.path)")
+			let cachedValues : Dictionary = content.resourceValuesForKeys([NSURLCreationDateKey], error: nil)!
+			let fileCreateDate : NSDate = cachedValues[NSURLCreationDateKey] as NSDate
+			let source = CGImageSourceCreateWithURL(content, nil)
+			if (nil == source) { continue }
+			imageFileData.append([
+				"FileName": content.lastPathComponent,
+				"ImageDate": getDateTime(source),
+				"FileDate": fileCreateDate
+				]
+			)
+			if (index < 10) {
+				fileListTableView.reloadData()
+			}
+		}
+		progressIndicator.stopAnimation(self)
+		
+	}
+	
   func parse(dateStr:String, format:String="yyyy-MM-dd") -> NSDate {
     var dateFmt = NSDateFormatter()
     dateFmt.timeZone = NSTimeZone(forSecondsFromGMT: 0)
@@ -146,18 +148,16 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDataSource, NSTab
   }
   
   func getDateTime(imageSource:CGImageSource) -> NSDate {
-    // return parse("2000:01:01 00:00:00", format:"yyyy:MM:dd HH:mm:ss")
     let uint:UInt = UInt.min
     let metadataAtIndex = CGImageSourceCopyMetadataAtIndex(imageSource, uint, nil)
     var imageDict : Dictionary  = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil)
-//    for value in imageDict.keys {
-//      println("value: \(value)")
-//    }
     var tiff : AnyObject = imageDict["{TIFF}"]! // __NSCFDictionary
-    println("TypeName of tiff = \(_stdlib_getTypeName(tiff))")
+    //println("TypeName of tiff = \(_stdlib_getTypeName(tiff))")
     //
-    let dateTime : String = tiff["DateTime"] as String!
-    return parse(dateTime, format:"yyyy:MM:dd HH:mm:ss")
+    if let dt = tiff["DateTime"] as? String {
+        return parse(dt, format:"yyyy:MM:dd HH:mm:ss")
+    }
+    return parse("2000:01:01 00:00:00", format:"yyyy:MM:dd HH:mm:ss")
   }
 
 }
