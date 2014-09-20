30c9939cedb7d6f721336d39bcd2cb3b9eeed5aa
Jürgen <juergenbaumann@gmail.com>
2014-09-20 17:04:25 +0200

    - formatted

--- ImageUtil/AppDelegate.swift
+++ ImageUtil/AppDelegate.swift
@@ -9,70 +9,70 @@
 import Cocoa
 
 class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDataSource, NSTableViewDelegate {
-
-  var imageFolder : NSURL?
-  var imageFileData : [NSDictionary] = []
-  let fm = NSFileManager.defaultManager()
-  
-  
-  @IBOutlet weak var window: NSWindow!
-  @IBOutlet weak var debugLog: NSTextField!
-  @IBOutlet weak var fileListTableView: NSTableView!
-  @IBOutlet weak var progressIndicator: NSProgressIndicator!
-  @IBOutlet weak var dateFromSelector: NSMatrix!
-  @IBOutlet weak var useNewStartDate: NSButton!
-  @IBOutlet weak var newStartDate: NSDatePicker!
-  @IBOutlet weak var hours: NSTextField!
-  @IBOutlet weak var postfix: NSTextField!
-  @IBOutlet weak var appendOriginalName: NSButton!
-
-  func applicationDidFinishLaunching(aNotification: NSNotification?) {
-    // Insert code here to initialize your application
-  }
-
-  func applicationWillTerminate(aNotification: NSNotification?) {
-    // Insert code here to tear down your application
-  }
-
-  @IBAction func selectImageFolder(sender: NSButton) {
-    var openPanel = NSOpenPanel()
-    openPanel.canChooseDirectories = true
-    openPanel.canChooseFiles = false
-    openPanel.canCreateDirectories = false
-    openPanel.allowsMultipleSelection = false
-    
-    if openPanel.runModal() == NSOKButton    {
-      imageFolder = openPanel.URLs[0] as? NSURL
-      let x : NSURL = openPanel.directoryURL
-      println("openPanel: \(openPanel.URLs)")
-      println("imageFolder: \(imageFolder)")
-      readMetaDataOfFilesInDirectory(imageFolder!)
-      fileListTableView.reloadData()
-      
-    }
-    let path : String! = imageFolder?.path!
-    debugLog.stringValue = "Pfad: \(path)"
-  }
-  
-  func numberOfRowsInTableView(aTableView: NSTableView!) -> Int {
-    return imageFileData.count
-  }
-  
-  func tableView(tableView: NSTableView, viewForTableColumn: NSTableColumn, row: Int) -> NSView {
-    let identifier = viewForTableColumn.identifier
-    var cell = fileListTableView.makeViewWithIdentifier(identifier, owner: self) as NSTableCellView
-    var value: AnyObject? = imageFileData[row].objectForKey(identifier)
-    if (value! is NSDate) {
-      let x = value as NSDate
-      cell.textField.stringValue = x.description
-    } else if (value is String) {
-      cell.textField.stringValue = value as String
-    } else {
-      cell.textField.stringValue = "./."
-    }
-    return cell;
-  }
-  
+	
+	var imageFolder : NSURL?
+	var imageFileData : [NSDictionary] = []
+	let fm = NSFileManager.defaultManager()
+	
+	
+	@IBOutlet weak var window: NSWindow!
+	@IBOutlet weak var debugLog: NSTextField!
+	@IBOutlet weak var fileListTableView: NSTableView!
+	@IBOutlet weak var progressIndicator: NSProgressIndicator!
+	@IBOutlet weak var dateFromSelector: NSMatrix!
+	@IBOutlet weak var useNewStartDate: NSButton!
+	@IBOutlet weak var newStartDate: NSDatePicker!
+	@IBOutlet weak var hours: NSTextField!
+	@IBOutlet weak var postfix: NSTextField!
+	@IBOutlet weak var appendOriginalName: NSButton!
+	
+	func applicationDidFinishLaunching(aNotification: NSNotification?) {
+		// Insert code here to initialize your application
+	}
+	
+	func applicationWillTerminate(aNotification: NSNotification?) {
+		// Insert code here to tear down your application
+	}
+	
+	@IBAction func selectImageFolder(sender: NSButton) {
+		var openPanel = NSOpenPanel()
+		openPanel.canChooseDirectories = true
+		openPanel.canChooseFiles = false
+		openPanel.canCreateDirectories = false
+		openPanel.allowsMultipleSelection = false
+		
+		if openPanel.runModal() == NSOKButton    {
+			imageFolder = openPanel.URLs[0] as? NSURL
+			let x : NSURL = openPanel.directoryURL
+			println("openPanel: \(openPanel.URLs)")
+			println("imageFolder: \(imageFolder)")
+			readMetaDataOfFilesInDirectory(imageFolder!)
+			fileListTableView.reloadData()
+			
+		}
+		let path : String! = imageFolder?.path!
+		debugLog.stringValue = "Pfad: \(path)"
+	}
+	
+	func numberOfRowsInTableView(aTableView: NSTableView!) -> Int {
+		return imageFileData.count
+	}
+	
+	func tableView(tableView: NSTableView, viewForTableColumn: NSTableColumn, row: Int) -> NSView {
+		let identifier = viewForTableColumn.identifier
+		var cell = fileListTableView.makeViewWithIdentifier(identifier, owner: self) as NSTableCellView
+		var value: AnyObject? = imageFileData[row].objectForKey(identifier)
+		if (value! is NSDate) {
+			let x = value as NSDate
+			cell.textField.stringValue = x.description
+		} else if (value is String) {
+			cell.textField.stringValue = value as String
+		} else {
+			cell.textField.stringValue = "./."
+		}
+		return cell;
+	}
+	
 	@IBAction func rename(sender: NSButton) {
 		println("dateFromSelector: \(dateFromSelector.selectedRow)")
 		println("useNewStartDate: \(useNewStartDate.state)")
@@ -110,11 +110,11 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDataSource, NSTab
 		let count = contents.count
 		imageFileData = [] // used to avoid duplicate new file names
 		for (index, url : NSURL) in enumerate(contents) {
-      // do we have an image?
-      if (!contains(["jped", "jpg"], (url.pathExtension as NSString).lowercaseString)) {
-        continue
-      }
-      println("\(url.pathExtension)")
+			// do we have an image?
+			if (!contains(["jped", "jpg"], (url.pathExtension as NSString).lowercaseString)) {
+				continue
+			}
+			println("\(url.pathExtension)")
 			let progress : Double = 100 * (Double(index + 1) / Double(count))
 			//println("progress: \(progress)")
 			progressIndicator.doubleValue = progress
@@ -137,32 +137,32 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDataSource, NSTab
 		
 	}
 	
-  func parse(dateStr:String, format:String="yyyy-MM-dd") -> NSDate {
-    var dateFmt = NSDateFormatter()
-    dateFmt.timeZone = NSTimeZone(forSecondsFromGMT: 0)
-    dateFmt.dateFormat = format
-    let date = dateFmt.dateFromString(dateStr)!
-    //println("\(dateStr) -> \(date)")
-    return date
-  }
-  
-  func stringFromDate(date:NSDate) -> String {
-    let dateStringFormatter = NSDateFormatter()
-    dateStringFormatter.dateFormat = "yyyy-MM-dd_HH-mm"
-    return dateStringFormatter.stringFromDate(date)
-  }
-  
-  func getDateTime(imageSource:CGImageSource) -> NSDate {
-    let uint:UInt = UInt.min
-    let metadataAtIndex = CGImageSourceCopyMetadataAtIndex(imageSource, uint, nil)
-    if let imageDict : Dictionary  = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil) {
-      var tiff : AnyObject = imageDict["{TIFF}"]! // __NSCFDictionary
-      if let dt = tiff["DateTime"] as? String {
-        return parse(dt, format:"yyyy:MM:dd HH:mm:ss")
-      }
-    }
-    return parse("2000:01:01 00:00:00", format:"yyyy:MM:dd HH:mm:ss")
-  }
-
+	func parse(dateStr:String, format:String="yyyy-MM-dd") -> NSDate {
+		var dateFmt = NSDateFormatter()
+		dateFmt.timeZone = NSTimeZone(forSecondsFromGMT: 0)
+		dateFmt.dateFormat = format
+		let date = dateFmt.dateFromString(dateStr)!
+		//println("\(dateStr) -> \(date)")
+		return date
+	}
+	
+	func stringFromDate(date:NSDate) -> String {
+		let dateStringFormatter = NSDateFormatter()
+		dateStringFormatter.dateFormat = "yyyy-MM-dd_HH-mm"
+		return dateStringFormatter.stringFromDate(date)
+	}
+	
+	func getDateTime(imageSource:CGImageSource) -> NSDate {
+		let uint:UInt = UInt.min
+		let metadataAtIndex = CGImageSourceCopyMetadataAtIndex(imageSource, uint, nil)
+		if let imageDict : Dictionary  = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil) {
+			var tiff : AnyObject = imageDict["{TIFF}"]! // __NSCFDictionary
+			if let dt = tiff["DateTime"] as? String {
+				return parse(dt, format:"yyyy:MM:dd HH:mm:ss")
+			}
+		}
+		return parse("2000:01:01 00:00:00", format:"yyyy:MM:dd HH:mm:ss")
+	}
+	
 }
 
